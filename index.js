3647f9be7ad00b898c44003fa298cf2413f16a0b
Mats Rörbecker <mats@rorbecker.com>
2022-11-25 16:20:20 +0100

    Removes logItems function

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
 
+/* eslint-disable no-console */
+
 "use strict";
 
 const fetchFeed = require("./lib/fetchFeed");
 const { fetchAllFeeds } = require("./lib/fetchAllFeeds");
+const formatItem = require("./lib/formatItem");
 const { parseFeed } = require("./lib/parseFeed");
-const logItems = require("./lib/logItems");
 
 const FETCH_INTERVAL = 30000;
 let ignoreNewsBefore = new Date(0);
+let firstTime = true;
 
 const sections = process.argv.slice(2);
 
@@ -22,7 +25,12 @@ const poll = async () => {
   const uniqueItems = [ ...new Map(items.map((item) => [ item.link, item ])).values() ];
   if (!uniqueItems.length) return;
   ignoreNewsBefore = uniqueItems[uniqueItems.length - 1].pubDate;
-  logItems(uniqueItems);
+  if (firstTime) {
+    console.log("\n");
+    console.log("SENASTE NYTT FRÅN EXPRESSEN:");
+    firstTime = false;
+  }
+  uniqueItems.forEach((item) => console.log(formatItem(item)));
 };
 
 setInterval(poll, FETCH_INTERVAL);

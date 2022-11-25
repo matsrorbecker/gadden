7638b1d3a221721bf572f6552a19307ab4b88439
Mats Rörbecker <mats@rorbecker.com>
2022-11-25 13:34:51 +0100

    Adds tests, fixes some whitespace issues

--- test/parseFeedTest.js
+++ test/parseFeedTest.js
@@ -2,15 +2,15 @@
 
 const fs = require("fs");
 const path = require("path");
-const parseFeed = require("../lib/parseFeed");
+const {
+  parseFeed,
+  missingSpacePattern,
+} = require("../lib/parseFeed");
 
 describe("parseFeed", () => {
-  let items;
-  before(() => {
-    const filePath = path.join(__dirname, "/data/nyheter.rss");
-    const nyheter = fs.readFileSync(filePath, "utf-8");
-    items = parseFeed(nyheter);
-  });
+  const filePath = path.join(__dirname, "/data/nyheter.rss");
+  const nyheter = fs.readFileSync(filePath, "utf-8");
+  const items = parseFeed(nyheter);
 
   it("parses all items in feed", () => {
     expect(items.length).to.equal(20);
@@ -25,6 +25,24 @@ describe("parseFeed", () => {
       expect(item.link).to.exist;
       expect(item.isPremium).to.exist;
       expect(typeof item.isPremium).to.equal("boolean");
+    });
+  });
+
+  it("replaces occurrences of '&nbsp;' entity with space", () => {
+    items.forEach((item) => {
+      expect(/&nbsp;/.test(item.description), item.description).to.be.false;
+    });
+  });
+
+  it("replaces occurrences of consecutive spaces with one space", () => {
+    items.forEach((item) => {
+      expect(/\s\s/.test(item.description), item.description).to.be.false;
+    });
+  });
+
+  it("adds space between sentences in description if missing", () => {
+    items.forEach((item) => {
+      expect(missingSpacePattern.test(item.description)).to.be.false;
     });
   });
 });
